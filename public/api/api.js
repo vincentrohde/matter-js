e82254ce1e01ce04b08d2d72a77ad25ebdae860c
Vincent Rohde <vincent.rohde@icloud.com>
2018-12-01 23:29:50 +0100

    add class structure to api

--- public/api/api.js
+++ public/api/api.js
@@ -1,23 +1,23 @@
 // custom functions
 
-let getPosition = (body) => {
-    return body.position;
+const getPosition = body => {
+  return body.position;
 };
 
-let getDifference = (original, input) => {
-    let difference = Math.abs(original - input);
-    if(original > input) {
-        return -difference;
-    } else {
-        return difference;
-    }
-}
+const getDifference = (original, input) => {
+  let difference = Math.abs(original - input);
+  if (original > input) {
+    return -difference;
+  } else {
+    return difference;
+  }
+};
 
-let updatePosition = (body, input) => {
-    return {
-        x: getDifference(body.x, input.x),
-        y: getDifference(body.y, input.y)
-    }
+const updatePosition = (body, input) => {
+  return {
+    x: getDifference(body.x, input.x),
+    y: getDifference(body.y, input.y)
+  };
 };
 
 // Matter.Body.translate(boxA, updatePosition(getPosition(boxA), input));
@@ -26,67 +26,96 @@ let updatePosition = (body, input) => {
 
 let boxAngularVelocity = 0;
 
-Matter.Events.on(engine, "afterUpdate", ()=>{
-    if(boxAngularVelocity > 0){
-        Matter.Body.setAngularVelocity( boxA, boxAngularVelocity);
-    }
-})
+Matter.Events.on(engine, "afterUpdate", () => {
+  if (boxAngularVelocity > 0) {
+    Matter.Body.setAngularVelocity(boxA, boxAngularVelocity);
+  }
+});
 
-const ChairAPI = {
-    rotate : velocity => {
-        boxAngularVelocity = velocity * Math.PI/6;
-    },
-    forward: velocity => Matter.Body.setVelocity( boxA, {x: 0, y: velocity * 10}),
-    stop: () => boxAngularVelocity = 0
-};
-
-const ChairControl = {
-    getChairs: () => {
-        return [Chair];
-    }
+class ChairAPI {
+  constructor(body, angularVelocity) {
+    this.body = body;
+    this.angularVelocity = angularVelocity;
+  }
+  rotate(velocity) {
+    angularVelocity = (velocity * Math.PI) / 6;
+  }
+  forward(velocity) {
+    Matter.Body.setVelocity(body, { x: 0, y: velocity * 10 });
+  }
+  stop() {
+    angularVelocity = 0;
+  }
 }
 
-const Chair = {
-    readyState: true,
-    batteryState: {
-        battery: 1.0
-    },
-    ready() {
-        return this.readyState;
-    },
-    getStatus() {
-        return this.batteryState;
-    },
-    move({motionType, velocity}) {
-        switch(motionType) {
-            case 'Rotation':
-                boxAngularVelocity = velocity * Math.PI/6;
-                break;
+class ChairControl {
+  constructor(chair) {
+    this.chair = chair;
+  }
+  getChairs() {
+    return [chair];
+  }
+}
 
-            case 'Straight':
-                const alpha = this.getPosition().bearing-90;
-                const a = Math.sin(alpha) * velocity;
-                const b = Math.cos(alpha) * velocity;
-                Matter.Body.applyForce(boxA, this.getPosition(), {x: 0.5, y: 0});
-                break;
-        }
-    },
+class Chair {
+  constructor(readyState, batteryState, motionState) {
+    this.readyState = readyState;
+    this.batteryState = batteryState;
+    this.motionState = motionState;
+  }
 
-    stop: () => {
-        boxAngularVelocity = 0;
-    },
+  ready() {
+    return this.readyState;
+  }
 
-    getPosition() {
+  getStatus() {
+    return this.batteryState;
+  }
 
-        return {
-            x: boxA.position.x,
-            y: boxA.position.y,
-            bearing: boxA.angle
-        }
+  move({ motionType, velocity }) {
+    switch (motionType) {
+      case "Rotation":
+        boxAngularVelocity = (velocity * Math.PI) / 6;
+        break;
 
+      case "Straight":
+        const alpha = this.getPosition().bearing - 90;
+        const a = Math.sin(alpha) * velocity;
+        const b = Math.cos(alpha) * velocity;
+        Matter.Body.applyForce(boxA, this.getPosition(), { x: 0.5, y: 0 });
+        break;
     }
-};
+  }
 
-window.ChairControl = ChairControl;
-const [chair] = ChairControl.getChairs();
+  stop() {
+    boxAngularVelocity = 0;
+  }
+
+  getPosition() {
+    return (
+      {
+        x: boxA.position.x,
+        y: boxA.position.y,
+        bearing: boxA.angle
+      } || null
+    );
+  }
+
+  getCurrentMotion() {
+    return this.motionState || null;
+  }
+}
+
+let myChairAPI = new ChairAPI(boxA, boxAngularVelocity);
+
+let chair = new Chair(
+  true,
+  {
+    battery: 1.0
+  },
+  "Straight"
+);
+
+// window.ChairControl = ChairControl;
+// const [chair] = ChairControl.getChairs();
 window.chair = chair;
